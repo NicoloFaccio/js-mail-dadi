4f2dd83a6c038e14bf0dfd7b31da80fc4ed53c94
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-10 14:55:55 +0100

    feat:working on the addeventlistener of the button

--- mail/assets/javascript/main.js
+++ mail/assets/javascript/main.js
@@ -1,5 +1,16 @@
 //INITIALISATION OF THE VARIABLES 
-const validationNameHtml=document.getElementById(emailValidation)
-const buttonValidation=document.getElementById(btnValidation)
+const validationNameHtml=document.getElementById(emailValidation);
+const buttonValidation=document.getElementById(btnValidation);
 
+//ARRAY'S SECTION
+const emails = ["figliouno@gmail.com", "figliodue@gmail.com", "figliotre@gmail.com", "figlioquattro@gmail.com", "figliocinue@gmail.com"]
 
+//FUNCTION'S SECTION
+
+for( let i = 0; i < emails.length; i++ ){
+    console.log( emails[i] )
+}
+
+buttonValidation.addEventListener("click", function(){
+    console.log(validationNameHtml.value)
+})
