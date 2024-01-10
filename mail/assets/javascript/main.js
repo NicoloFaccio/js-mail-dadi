e4684160e8fd4d8e0401a05a3ae1a2772c59472b
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-10 16:00:03 +0100

    feat: working on the cycle for chacking the email

--- mail/assets/javascript/main.js
+++ mail/assets/javascript/main.js
@@ -1,16 +1,28 @@
 //INITIALISATION OF THE VARIABLES 
-const validationNameHtml=document.getElementById(emailValidation);
-const buttonValidation=document.getElementById(btnValidation);
+const validationNameHtml=document.getElementById("emailValidation");
+const buttonValidation=document.getElementById("btnValidation");
+const resultEmail=document.getElementById("result");
 
 //ARRAY'S SECTION
 const emails = ["figliouno@gmail.com", "figliodue@gmail.com", "figliotre@gmail.com", "figlioquattro@gmail.com", "figliocinue@gmail.com"]
 
 //FUNCTION'S SECTION
 
-for( let i = 0; i < emails.length; i++ ){
-    console.log( emails[i] )
-}
-
 buttonValidation.addEventListener("click", function(){
     console.log(validationNameHtml.value)
+
+    let checkingMail = false
+
+    for( let i = 0; i < emails.length; i++ ){
+        if(emails[i]==="figliouno@gmail.com"){
+            checkingMail = true 
+        }
+    }
+
+    if (checkingMail = true){
+        resultEmail.innerHTML += "La mail è esistente"
+    } else {
+        resultEmail.innerHTML += "La mail non è esistente" 
+    }
+    
 })
