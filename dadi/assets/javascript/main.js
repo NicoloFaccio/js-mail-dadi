2bc8c74e3624b180749156832773ac413ef372ad
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-10 17:33:13 +0100

    fix: canceled the console.log

--- dadi/assets/javascript/main.js
+++ dadi/assets/javascript/main.js
@@ -4,12 +4,20 @@ const randomUser = document.getElementById("userNumber")
 const randomWinner = document.getElementById("resultWinner")
 
 //FUNCTION'S SECTION
-let computer = RandomNumber (1,7)
-let gamer = RandomNumber (1,7)
+let computer = RandomNumber (1,6)
+let gamer = RandomNumber (1,6)
 
 randomPc.innerHTML = computer
 randomUser.innerHTML = gamer
 
 function RandomNumber( min , max ) {
     return Math.floor( Math.random()*(max-min + 1) + 1 )
+}
+
+if (computer > gamer){
+    randomWinner.innerHTML=("Il computer ha vinto")
+} else if (computer < gamer) {
+    randomWinner.innerHTML=("Il giocatore ha vinto")
+} else if (computer === gamer) {
+    randomWinner.innerHTML=("Ricaricare la pagina")
 }
