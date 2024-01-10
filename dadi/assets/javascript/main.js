4f7c388cf2e3e46e7a5a522a3c1788f434f04959
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-10 17:07:38 +0100

    feat: created the  random number's function

--- dadi/assets/javascript/main.js
+++ dadi/assets/javascript/main.js
@@ -4,3 +4,12 @@ const randomUser = document.getElementById("userNumber")
 const randomWinner = document.getElementById("resultWinner")
 
 //FUNCTION'S SECTION
+let computer = RandomNumber (1,7)
+let gamer = RandomNumber (1,7)
+
+randomPc.innerHTML = computer
+randomUser.innerHTML = gamer
+
+function RandomNumber( min , max ) {
+    return Math.floor( Math.random()*(max-min + 1) + 1 )
+}
