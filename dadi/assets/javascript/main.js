6700b0060d3a88d1b0e80fa632e9a7bb5afca342
NicoloFaccio <nicolo.faccio1@gmail.com>
2024-01-10 16:32:15 +0100

    feat: created the variables of the game's dice

---
+++ dadi/assets/javascript/main.js
@@ -0,0 +1,6 @@
+//INITIALISATION OF THE VARIABLES 
+const randomPc = document.getElementById("computerNumber")
+const randomUser = document.getElementById("userNumber")
+const randomWinner = document.getElementById("resultWinner")
+
+//FUNCTION'S SECTION
